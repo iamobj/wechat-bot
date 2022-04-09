ee7c969116704d4447d1ee02a120a1e9847caf16
iamobj <twboss8@126.com>
2022-04-10 00:37:32 +0800

    perf: 基友群推送增加是否工作日判断及其它一些优化

--- src/plugins/wechat-bot/wechat/task/timerSend.js
+++ src/plugins/wechat-bot/wechat/task/timerSend.js
@@ -1,12 +1,20 @@
 // 定时器，定时推送消息
 import schedule from 'node-schedule'
+import axios from 'axios'
 
-const timerSend = ({ wechatBot, CODES, targetKey, targetValue, content, timeRule }) => {
-  schedule.scheduleJob(timeRule, () => {
+const timerSend = ({ wechatBot, onBeforeSend, CODES, targetKey, targetValue, content, timeRule }) => {
+  schedule.scheduleJob(timeRule, async() => {
+    if (onBeforeSend) {
+      const flag = await onBeforeSend()
+      // 如果返回false，则不发送
+      if (!flag) return
+    }
+    const _content = typeof content === 'function' ? content() : content
+
     wechatBot.sendByTarget({
       targetKey: targetKey,
       targetValue: targetValue,
-      content
+      content: _content
     })
   })
 }
@@ -17,15 +25,26 @@ export default timerSend
  * 基友群基金操作定时推送，每周一到周五14点40分提醒操作基金
  */
 export const JiYouGroupPush = wechatBot => {
-  const contentPool = [
-    '[imgMsg]C:\\Users\\iamc\\Desktop\\wechat-bot-imgs\\2.jpg',
-    '2点40啦，请各位老板系好安全带，准备起飞！！！！',
-    '2点40啦，祝各位老板起飞！！！！',
-    '2点40啦，老板们起飞了别忘记打赏我哦'
-  ]
+  const contentFn = () => {
+    const contentPool = [
+      '[imgMsg]C:\\Users\\iamc\\Desktop\\wechat-bot-imgs\\2.jpg',
+      '2点40啦，请各位老板系好安全带，准备起飞！！！！',
+      '2点40啦，祝各位老板起飞！！！！',
+      '2点40啦，老板们起飞了别忘记打赏我哦'
+    ]
+    return contentPool[Math.floor(Math.random() * contentPool.length)]
+  }
 
   timerSend({
     wechatBot,
+    onBeforeSend: async() => {
+      // 判断是否是工作日，避免节假日推送
+      const { data } = await axios.get('https://timor.tech/api/holiday/info')
+      if (data?.type?.type === 0) {
+        return true
+      }
+      return false
+    },
     timeRule: {
       dayOfWeek: [new schedule.Range(1, 5)],
       hour: 14,
@@ -33,6 +52,6 @@ export const JiYouGroupPush = wechatBot => {
     },
     targetKey: 'remarks',
     targetValue: '熊猫阁基友群',
-    content: contentPool[Math.floor(Math.random() * contentPool.length)]
+    content: contentFn
   })
 }
