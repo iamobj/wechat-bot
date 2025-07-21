66d93f3c1728dd17539e8e21d0bab813c41eab1c
iamobj <twboss8@126.com>
2025-07-21 22:48:17 +0800

    chore: 更换机器人通信地址

--- src/plugins/wxhelper/index.js
+++ src/plugins/wxhelper/index.js
@@ -6,21 +6,24 @@ const defaults = {
   instance: 'wxhelper',
 }
 
-export default fp(async (fastify, opts, done) => {
-  const wxhelperIns = new Wxhelper('http://192.168.50.115:29088/api')
-  if (process.env.NODE_ENV !== 'development') {
-    const isLogin = await wxhelperIns.checkLogin()
-    if (isLogin) {
-      await wxhelperIns.sendTextMsg({
-        wxid: 'wxid_d52rxumg20z022',
-        msg: `【${process.env.NODE_ENV}】微信机器人服务启动成功`,
-      })
+export default fp(
+  async (fastify, opts, done) => {
+    const wxhelperIns = new Wxhelper('http://192.168.50.16:19088/api')
+    if (process.env.NODE_ENV !== 'development') {
+      const isLogin = await wxhelperIns.checkLogin()
+      if (isLogin) {
+        await wxhelperIns.sendTextMsg({
+          wxid: 'wxid_d52rxumg20z022',
+          msg: `【${process.env.NODE_ENV}】微信机器人服务启动成功`,
+        })
+      }
     }
+
+    fastify.decorate(defaults.instance, wxhelperIns)
+
+    done()
+  },
+  {
+    name: defaults.pluginName,
   }
-
-  fastify.decorate(defaults.instance, wxhelperIns)
-
-  done()
-}, {
-  name: defaults.pluginName,
-})
+)
